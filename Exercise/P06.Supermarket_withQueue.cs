ad610810fd89e1ca956fe9a7238638c4ca975914
VladimirPelov <vpelov@mail.bg>
2022-04-13 13:25:17 +0300

    Add files via upload

---
+++ Exercise/P06.Supermarket_withQueue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P06.Supermarket_withQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "End")
+                {
+                    break;
+                }
+                else if (command == "Paid" && queue.Count > 0)
+                {
+                    for (int i = 0; i < queue.Count; )
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(command);
+                }
+            }
+
+            Console.WriteLine($"{queue.Count} people remaining.");
+
+        }
+    }
+}
