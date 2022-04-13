71c06a472a4075944761c5ecde1dcd26db748462
VladimirPelov <vpelov@mail.bg>
2022-04-13 12:56:35 +0300

    Add files via upload

---
+++ Exercise/P05.PrintEvenNumbers_withQueue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05.PrintEvenNumbers_withQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            int[] inData = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            foreach (int item in inData)
+            {
+                if (item % 2 == 0)
+                {
+                    queue.Enqueue(item);
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", queue));
+        }
+    }
+}
