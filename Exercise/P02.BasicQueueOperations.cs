7b6b0e00c6b6da44a9a67df82d5a816a231da34a
VladimirPelov <vpelov@mail.bg>
2022-04-14 09:17:35 +0300

    Add files via upload

---
+++ Exercise/P02.BasicQueueOperations.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02.BasicQueueOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] controlDigits = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = controlDigits[0];
+            int s = controlDigits[1];
+            int x = controlDigits[2];
+
+            Queue<int> queueNumbers = new Queue<int>();
+
+            string number = Console.ReadLine();
+            int[] numbers = number
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < n; i++)
+            {
+                queueNumbers.Enqueue(numbers[i]);
+            }
+
+            if (queueNumbers.Count < s)
+            {
+                s = queueNumbers.Count;
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                queueNumbers.Dequeue();
+            }
+
+            if (queueNumbers.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else if (queueNumbers.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(queueNumbers.Min());
+            }
+        }
+    }
+}
