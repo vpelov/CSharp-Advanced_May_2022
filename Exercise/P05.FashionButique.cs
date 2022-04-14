43043e734f0cf9c647a4f36cc53b0b1ed02e0c80
VladimirPelov <vpelov@mail.bg>
2022-04-14 14:12:41 +0300

    Add files via upload

---
+++ Exercise/P05.FashionButique.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05.FashionButique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] clotesNum = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(byte.Parse)
+                .ToArray();
+
+            Stack<byte> clothesSteck = new Stack<byte>();
+            foreach (byte item in clotesNum)
+            {
+                clothesSteck.Push(item);
+            }
+
+            byte rackCapacity = byte.Parse(Console.ReadLine());
+            byte numberRack = 0;
+
+            while (clothesSteck.Count != 0)
+            {
+                byte currentRack = clothesSteck.Pop();
+                if (clothesSteck.Count == 0)
+                {
+                    numberRack++;
+                    break;
+                }
+
+                while (clothesSteck.Count != 0)
+                {
+
+                    byte item = clothesSteck.Peek();
+                    if (currentRack + item > rackCapacity)
+                    {
+                        numberRack++;
+                        break;
+                    }
+                    else
+                    {
+                        currentRack += clothesSteck.Pop();
+                        if (clothesSteck.Count == 0)
+                        {
+                            numberRack++;
+                            break;
+                        }
+                    }
+                
+                }
+            }
+
+            Console.WriteLine(numberRack);
+        }
+    }
+}
