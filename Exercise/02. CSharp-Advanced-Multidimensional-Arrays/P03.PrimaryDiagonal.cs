155d129b805c8ee3c28946d8d9ead6ac5e46fe00
VladimirPelov <vpelov@mail.bg>
2022-04-18 12:33:26 +0300

    Add files via upload

---
+++ Exercise/02. CSharp-Advanced-Multidimensional-Arrays/P03.PrimaryDiagonal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace P03.PrimaryDiagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];                  // Read input Matrix 
+           
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                int[] currantArr = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int columns = 0; columns < matrix.GetLength(1); columns++)
+                {
+                    int num = currantArr[columns];
+                    matrix[rows, columns] = num;
+                }
+
+            }
+
+            int sum = 0;                            // Operation
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int columns = rows; columns < rows + 1; columns++)
+                {
+                    int number = matrix[rows, columns];
+                    sum += number;
+                }
+            }
+
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
