c6fdf14521524de09e27f132d71592e510c2063b
VladimirPelov <vpelov@mail.bg>
2022-04-18 15:14:48 +0300

    Add files via upload

---
+++ Exercise/02. CSharp-Advanced-Multidimensional-Arrays/P04.SymbolInMatrix.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace P04.SymbolInMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                char[] currentChar = Console.ReadLine().ToCharArray();
+
+                for (int columns = 0; columns < matrix.GetLength(1); columns++)
+                {
+                    char ch = currentChar[columns];
+                    matrix[rows, columns] = ch;
+                }
+            }
+
+
+            int row;
+            int column;
+            bool isFind = true;
+            char[] symbol = Console.ReadLine().ToCharArray();
+            char searchCh = symbol[0];
+           
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int columns = 0; columns < matrix.GetLength(1); columns++)
+                {
+                    char currentChar = matrix[rows, columns];
+                    if (currentChar == searchCh)
+                    {
+                        row = rows;
+                        column = columns;
+                        Console.WriteLine($"({row}, {column})");
+                        isFind =false;
+                        break;
+                    }
+                                        
+                }
+                if (isFind == false)
+                {
+                    break;
+                }
+            }
+
+
+            if (isFind)
+            {
+                Console.WriteLine($"{searchCh} does not occur in the matrix ");
+            }
+
+        }
+    }
+}
