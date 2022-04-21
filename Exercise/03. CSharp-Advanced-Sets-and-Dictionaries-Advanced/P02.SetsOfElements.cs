bdccfb37c443cbb503658d538fe75d358ad21eb6
VladimirPelov <vpelov@mail.bg>
2022-04-21 09:47:15 +0300

    Add files via upload

---
+++ Exercise/03. CSharp-Advanced-Sets-and-Dictionaries-Advanced/P02.SetsOfElements.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02.SetsOfElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] lengthSets = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int n = lengthSets[0];
+            int m = lengthSets[1];
+
+            HashSet<string> firstSet = new HashSet<string>();
+            HashSet<string> secondSet = new HashSet<string>();
+            HashSet<string> print = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string element = Console.ReadLine();
+                firstSet.Add(element);
+            }
+            for (int i = 0; i < m; i++)
+            {
+                string element = Console.ReadLine();
+                secondSet.Add(element);
+            }
+
+            foreach (string item in firstSet)
+            {
+                foreach (string data in secondSet)
+                {
+                    if (item == data)
+                    {
+                        Console.Write($"{item} ");
+                    }
+                }
+            }
+
+
+        }
+    }
+}
