e4ca4e08e7b02afa8395c5b40ea9325b78ede2fe
VladimirPelov <vpelov@mail.bg>
2022-04-21 09:02:40 +0300

    Add files via upload

---
+++ Exercise/03. CSharp-Advanced-Sets-and-Dictionaries-Advanced/P01.UniqueUsername.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P01.UniqueUsername
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> dataUserName = new HashSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                dataUserName.Add(name); 
+            }
+
+            foreach (string item in dataUserName)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
