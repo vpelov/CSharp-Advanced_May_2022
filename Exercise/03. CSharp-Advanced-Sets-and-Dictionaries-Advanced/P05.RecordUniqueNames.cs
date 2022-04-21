2d615f234461226d0ea664d0421bda7ae47d6484
VladimirPelov <vpelov@mail.bg>
2022-04-21 05:56:11 +0300

    Add files via upload

---
+++ Exercise/03. CSharp-Advanced-Sets-and-Dictionaries-Advanced/P05.RecordUniqueNames.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P05.RecordUniqueNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> namesStore = new HashSet<string>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                namesStore.Add(name);
+            }
+
+            foreach (string item in namesStore)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
