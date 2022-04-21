07225f2391a983180ed1bf8e358f206397ac09b5
VladimirPelov <vpelov@mail.bg>
2022-04-21 10:22:27 +0300

    Add files via upload

---
+++ Exercise/03. CSharp-Advanced-Sets-and-Dictionaries-Advanced/P05.EvenTimes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P05.EvenTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<int> sequenseNumber = new HashSet<int>();
+            int number = 234;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (!sequenseNumber.Contains(num))
+                {
+                    sequenseNumber.Add(num);
+                }
+                else
+                {
+                    number = num;
+                }
+            }
+
+            if (number != 234)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
