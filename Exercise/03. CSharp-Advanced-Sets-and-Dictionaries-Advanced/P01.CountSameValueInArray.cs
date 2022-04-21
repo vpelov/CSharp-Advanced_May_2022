e90f31c2c08e8b12161f71a927a5b5e12731375f
VladimirPelov <vpelov@mail.bg>
2022-04-21 04:06:36 +0300

    Add files via upload

---
+++ Exercise/03. CSharp-Advanced-Sets-and-Dictionaries-Advanced/P01.CountSameValueInArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.CountSameValueInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] inArray = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            HashSet<string> test = new HashSet<string>();
+            Dictionary<double, int> dataDict = new Dictionary<double, int>();
+
+            for (int i = 0; i < inArray.Length; i++)
+            {
+                if (!dataDict.ContainsKey(inArray[i]))
+                {
+                    dataDict.Add(inArray[i], 1);
+                }
+                else
+                {
+                    dataDict[inArray[i]]++;
+                }
+            }
+
+            foreach (var item in dataDict)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+
+        }
+    }
+}
