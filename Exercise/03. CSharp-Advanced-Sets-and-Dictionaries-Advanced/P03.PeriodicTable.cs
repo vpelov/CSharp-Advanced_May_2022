e6c01f26140a055b28627f2b3afb3032ade7e1b4
VladimirPelov <vpelov@mail.bg>
2022-04-21 10:04:29 +0300

    Add files via upload

---
+++ Exercise/03. CSharp-Advanced-Sets-and-Dictionaries-Advanced/P03.PeriodicTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03.PeriodicTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+           SortedSet<string> table = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string dataIn = Console.ReadLine();
+                string[] elements = dataIn.Split(' ');
+
+                for (int j = 0; j < elements.Length; j++)
+                {
+                    table.Add(elements[j]);
+                }
+            }
+
+          
+            foreach (string item in table)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
