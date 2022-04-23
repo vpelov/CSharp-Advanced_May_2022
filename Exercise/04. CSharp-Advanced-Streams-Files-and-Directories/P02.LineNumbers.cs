3db7d4a391a90f15e57a485c4101d3f8624ff710
VladimirPelov <vpelov@mail.bg>
2022-04-23 20:16:00 +0300

    Add files via upload

---
+++ Exercise/04. CSharp-Advanced-Streams-Files-and-Directories/P02.LineNumbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace P02.LineNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"input.txt";
+            string outputFilePath = @"output.txt";
+
+            ReWriteFile(inputFilePath, outputFilePath);
+
+        }
+
+        static void ReWriteFile(string inputFilePath, string outputfilePath)
+        {
+            using (StreamWriter sw = new StreamWriter(outputfilePath))
+            {
+                using (StreamReader sr = new StreamReader(inputFilePath))
+                {
+                    string line = sr.ReadLine();
+                    int count = 1;
+                    while (line != null)
+                    {
+                        sw.WriteLine($"{count}. {line}");
+                            count++;
+                        line = sr.ReadLine();
+                    }
+                }
+            }     
+        }
+    }
+}
---
+++ Exercise/04. CSharp-Advanced-Streams-Files-and-Directories/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace P01.OddLines
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader(@"input.txt"))
+            {
+                int count = 0;
+                var line = reader.ReadLine();
+
+                while (line != null)
+                {
+                    if (count % 2 == 1)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                    line = reader.ReadLine();
+                    count++;
+                }
+            }
+        }
+    }
+}
