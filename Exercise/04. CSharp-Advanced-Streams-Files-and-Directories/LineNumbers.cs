985f9563b5efd88b11a222261c41a1598fcf64c5
VladimirPelov <vpelov@mail.bg>
2022-04-24 12:00:56 +0300

    Add files via upload

---
+++ Exercise/04. CSharp-Advanced-Streams-Files-and-Directories/LineNumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace RewriteFileWithLineNumbers
+{
+    internal class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"input.txt";
+            string outputFilePath = @"output.txt";
+
+            ReWriteFile(inputFilePath, outputFilePath);
+        }
+
+        static void ReWriteFile(string inputFilePath, string outputfilePath)
+        {
+            using (StreamWriter sw = new StreamWriter(outputfilePath))
+            {
+                using (StreamReader sr = new StreamReader(inputFilePath))
+                {
+                    string line = sr.ReadLine();
+                    int count = 1;
+                    while (line != null)
+                    {
+                        sw.WriteLine($"{count}. {line}");
+                        count++;
+                        line = sr.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
+
+
