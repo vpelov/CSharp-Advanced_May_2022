a046a54b706c65adfcfc1abd3205b59e7c0c597f
VladimirPelov <vpelov@mail.bg>
2022-05-17 17:13:17 +0300

    Add files via upload

---
+++ C# Advanced/01. CSharp-Advanced-Stacks-and-Queues/P02.StackSum.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02.StackSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>(Console.ReadLine().Split(' ').Select(int.Parse));
+
+            string command = Console.ReadLine().ToLower();
+
+            while (command != "end")
+            {
+                string[] cmd = command
+                    .ToLower()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                if (cmd[0] == "add")
+                {
+                    int first = int.Parse(cmd[1]);
+                    int second = int.Parse(cmd[2]);
+                    stack.Push(first);
+                    stack.Push(second);
+                }
+                else if (cmd[0] == "remove")
+                {
+                    int number = int.Parse(cmd[1]);
+
+                    if (stack.Count >= number)
+                    {
+                        for (int i = 0; i < number; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+                command = Console.ReadLine().ToLower();
+            }
+
+            int totalSum = 0;
+
+            while (stack.Count > 0)
+            {
+                int num = stack.Pop();
+                totalSum += num;
+            }
+
+            Console.WriteLine($"Sum: {totalSum}");
+        }
+    }
+}
