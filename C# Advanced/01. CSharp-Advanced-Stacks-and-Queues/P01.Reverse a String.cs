71ea4c4e100a42d8208c04432f8315a98b238043
VladimirPelov <vpelov@mail.bg>
2022-05-17 16:23:22 +0300

    Add files via upload

---
+++ C# Advanced/01. CSharp-Advanced-Stacks-and-Queues/P01.Reverse a String.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P01.Reverse_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char item in str)
+            {
+                stack.Push(item);
+            }
+
+            foreach (char ch in stack)
+            {
+                Console.Write(ch);
+            }
+        }
+    }
+}
