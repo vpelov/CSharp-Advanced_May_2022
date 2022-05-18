fe7229678ecfa6d66fd154045dd4e7576f4eb72e
VladimirPelov <vpelov@mail.bg>
2022-05-18 10:48:20 +0300

    Add files via upload

---
+++ C# Advanced/01. CSharp-Advanced-Stacks-and-Queues/P06.Supermarket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P06.Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                if (command == "Paid")
+                {
+                    while (queue.Count > 0)
+                    {
+                        string printString = queue.Dequeue();
+                        Console.WriteLine(printString);
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(command);
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{queue.Count} people remaining.");
+
+
+        }
+    }
+}
