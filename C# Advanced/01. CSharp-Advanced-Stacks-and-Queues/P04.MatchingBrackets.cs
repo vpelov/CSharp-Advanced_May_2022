65c7093487ec706364708c4bf2a31e021076c54d
VladimirPelov <vpelov@mail.bg>
2022-05-18 10:26:08 +0300

    Add files via upload

---
+++ C# Advanced/01. CSharp-Advanced-Stacks-and-Queues/P04.MatchingBrackets.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04.MatchingBrackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            string data = Console.ReadLine();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (data[i] == ')')
+                {
+                    int startIndex = stack.Pop();
+                    int endIndex = i;
+                    Console.WriteLine(data.Substring(startIndex, endIndex - startIndex + 1));
+                }
+            }
+        }
+    }
+}
