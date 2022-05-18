a6092e9daed6fe12bfc075c392bf35abdfd45519
VladimirPelov <vpelov@mail.bg>
2022-05-18 10:38:05 +0300

    Add files via upload

---
+++ C# Advanced/01. CSharp-Advanced-Stacks-and-Queues/P05.PrintEvenNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05.PrintEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>(Console.ReadLine().Split(' ').Select(int.Parse));
+            Queue<int> printQueue = new Queue<int>();
+
+            foreach (int item in queue)
+            {
+                if (item % 2 == 0)
+                {
+                    printQueue.Enqueue(item);
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", printQueue));
+
+        }
+    }
+}
