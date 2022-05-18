5655150d3431865a0b710d486b5f7a0d1bd75255
VladimirPelov <vpelov@mail.bg>
2022-05-18 11:41:50 +0300

    Add files via upload

---
+++ C# Advanced/01. CSharp-Advanced-Stacks-and-Queues/P07.HotPotato.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07.HotPotato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inData = Console.ReadLine();
+            int numberCircle = int.Parse(Console.ReadLine());
+
+            string[] data = inData
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            Queue<string> queue = new Queue<string>();
+
+            foreach (string item in data)
+            {
+                queue.Enqueue(item);
+            }
+
+            while (queue.Count > 1)
+            {
+                for (int i = 0; i < numberCircle - 1; i++)
+                {
+                    string remove = queue.Dequeue();
+                    queue.Enqueue(remove);
+                }
+
+                string print = queue.Dequeue();
+                Console.WriteLine($"Removed {print}");
+            }
+
+            string last = queue.Peek();
+            Console.WriteLine($"Last is {last}");
+
+        }
+    }
+}
