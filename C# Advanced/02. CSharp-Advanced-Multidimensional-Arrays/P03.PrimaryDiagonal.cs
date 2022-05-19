bb83c75fa3434b46c591a469de9931c55ef1f00b
VladimirPelov <vpelov@mail.bg>
2022-05-19 08:21:28 +0300

    Add files via upload

---
+++ C# Advanced/02. CSharp-Advanced-Multidimensional-Arrays/P03.PrimaryDiagonal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace P03.PrimaryDiagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] array = Console.ReadLine()
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = array[j];
+                }
+            }
+
+            int totalSum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = i; j < i + 1; j++)
+                {
+                    totalSum += matrix[i, j];
+                }
+            }
+            Console.WriteLine(totalSum);
+        }
+    }
+}
