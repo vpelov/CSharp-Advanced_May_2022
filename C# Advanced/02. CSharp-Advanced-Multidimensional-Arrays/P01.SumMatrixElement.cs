a150672f41be2d307ce15ac3c8c64a55f4f040a4
VladimirPelov <vpelov@mail.bg>
2022-05-19 07:33:05 +0300

    Add files via upload

---
+++ C# Advanced/02. CSharp-Advanced-Multidimensional-Arrays/P01.SumMatrixElement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace P01.SumMatrixElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimension = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+            int row = dimension[0];
+            int col = dimension[1];
+            int[,] matrix = new int[row, col];          
+
+            for (int i = 0; i < row; i++)
+            {
+                int[] currentArray = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+                for (int j = 0; j < col; j++)
+                {
+                    matrix[i, j] = currentArray[j];
+                }
+            }
+
+            int totalSum = 0;
+            foreach (int elements in matrix)
+            {
+                totalSum += elements;
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(totalSum);
+
+        }
+    }
+}
