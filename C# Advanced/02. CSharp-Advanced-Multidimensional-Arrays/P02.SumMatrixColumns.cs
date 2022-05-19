5fe4b26e78e56dc1c79286d0ee74c9a459a21e21
VladimirPelov <vpelov@mail.bg>
2022-05-19 07:58:10 +0300

    Add files via upload

---
+++ C# Advanced/02. CSharp-Advanced-Multidimensional-Arrays/P02.SumMatrixColumns.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace P02.SumMatrixColumns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimension = Console.ReadLine()
+                .Split(',')
+                .Select(int.Parse)
+                .ToArray();
+            int row = dimension[0];
+            int col = dimension[1];
+            int[,] matrix = new int[row, col];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] currentArray = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = currentArray[j];
+                }
+            }
+
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                int currentSum = 0;
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    currentSum += matrix[j, i];
+                }
+                Console.WriteLine(currentSum);
+            }
+        }
+    }
+}
