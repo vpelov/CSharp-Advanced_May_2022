468df377b09908f57fd2c8ea169cdc41cce073ef
VladimirPelov <vpelov@mail.bg>
2022-05-20 05:58:28 +0300

    Add files via upload

---
+++ C# Advanced/02. CSharp-Advanced-Multidimensional-Arrays/P06.JaggedArrayModification.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace P06.JaggedArrayModification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            int[][] jaggedArray = new int[rows][];
+
+            for (int i = 0; i < jaggedArray.GetLength(0); i++)
+            {
+                int[] currentArray = Console.ReadLine()
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+                jaggedArray[i] = new int[currentArray.Length];
+                jaggedArray[i] = currentArray;
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                string[] cmd = command
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                int row = int.Parse(cmd[1]);
+                int col = int.Parse(cmd[2]);
+                int value = int.Parse(cmd[3]);
+
+                if (row < 0 || row > jaggedArray.Length - 1 || col < 0 || col > jaggedArray[row].Length - 1)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                    command = Console.ReadLine();
+                    continue;
+
+                }
+                else if (cmd[0] == "Add")
+                {                    
+                    jaggedArray[row][col] += value;
+                }
+                else if (cmd[0] == "Subtract")
+                {                    
+                    jaggedArray[row][col] -= value;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                Console.WriteLine(String.Join(' ', jaggedArray[i]));
+            }
+        }
+    }
+}
