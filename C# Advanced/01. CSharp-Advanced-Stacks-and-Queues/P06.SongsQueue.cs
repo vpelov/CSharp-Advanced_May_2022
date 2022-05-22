5d48636f036c4a3a260a51bcdf7d78418672e173
VladimirPelov <vpelov@mail.bg>
2022-05-22 17:04:08 +0300

    Add files via upload

---
+++ C# Advanced/01. CSharp-Advanced-Stacks-and-Queues/P06.SongsQueue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P06.SongsQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            Queue<string> queueWithSongs = new Queue<string>();
+            for (int i = 0; i < songs.Length; i++)
+            {
+                queueWithSongs.Enqueue(songs[i]);
+            }
+
+            string command = Console.ReadLine();
+            while (queueWithSongs.Count > 0)
+            {
+                string[] cmd = command
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                if (cmd[0] == "Play")
+                {
+                    queueWithSongs.Dequeue();
+                }
+                else if (cmd[0] == "Add")
+                {
+                    string newSong = command.Substring(4, command.Length - 4);
+                    if (queueWithSongs.Contains(newSong))
+                    {
+                        Console.WriteLine($"{newSong} is already contained!");
+                    }
+                    else
+                    {
+                        queueWithSongs.Enqueue(newSong);
+                    }
+                }
+                else if (cmd[0] == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", queueWithSongs));
+                }
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine("No more songs!");
+
+        }
+    }
+}
