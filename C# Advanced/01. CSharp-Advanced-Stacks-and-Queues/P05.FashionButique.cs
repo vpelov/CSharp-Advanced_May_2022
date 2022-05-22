0c79f56ff490cc2c3dc654b8fdc50aad489623cf
VladimirPelov <vpelov@mail.bg>
2022-05-22 16:35:22 +0300

    Add files via upload

---
+++ C# Advanced/01. CSharp-Advanced-Stacks-and-Queues/P05.FashionButique.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05.FashionButique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothesValue = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < clothesValue.Length; i++)
+            {
+                stack.Push(clothesValue[i]);
+            }
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int numberRack = 0;
+            int currentSum = 0;
+
+            while (stack.Count > 0)
+            {
+                currentSum += stack.Peek();
+                if (currentSum > rackCapacity)
+                {
+                    numberRack++;
+                    currentSum = 0;
+                }
+                else
+                {
+                    stack.Pop();
+                }                
+            }
+            if (currentSum != 0)
+            {
+                numberRack++;
+            }
+            Console.WriteLine(numberRack);
+        }
+    }
+}
