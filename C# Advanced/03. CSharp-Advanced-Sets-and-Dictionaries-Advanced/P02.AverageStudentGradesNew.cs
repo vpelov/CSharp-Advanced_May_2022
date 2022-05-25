c3ec976f434e7f48b74f534e5c7042b7cf2cebe3
VladimirPelov <vpelov@mail.bg>
2022-05-25 21:14:38 +0300

    Add files via upload

---
+++ C# Advanced/03. CSharp-Advanced-Sets-and-Dictionaries-Advanced/P02.AverageStudentGradesNew.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02.AverageStudentGradesNew
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<decimal>> dataDict = new Dictionary<string, List<decimal>>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] stringArr = Console.ReadLine()
+                    .Split(' ')
+                    .ToArray();
+                string name = stringArr[0];
+                decimal grade = decimal.Parse(stringArr[1]);
+                if (!dataDict.ContainsKey(name))
+                {
+                    List<decimal> current = new List<decimal>();
+                    current.Add(grade);
+                    dataDict.Add(name, current);
+                }
+                else
+                {
+                    dataDict[name].Add(grade);
+                }
+            }
+
+            foreach (var item in dataDict)
+            {
+                Console.Write($"{item.Key} -> ");
+                for (int i = 0; i < item.Value.Count; i++)
+                {
+                    Console.Write($"{item.Value[i]:f2} ");
+                }
+                Console.WriteLine($"(avg: { item.Value.Average():f2})");
+
+            }
+        }
+    }
+}
