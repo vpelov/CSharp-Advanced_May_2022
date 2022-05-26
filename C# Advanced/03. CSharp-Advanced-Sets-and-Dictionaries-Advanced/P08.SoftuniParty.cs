d725f7a85344cb6c5536a358df0a14e26e647b1c
VladimirPelov <vpelov@mail.bg>
2022-05-26 15:38:51 +0300

    Add files via upload

---
+++ C# Advanced/03. CSharp-Advanced-Sets-and-Dictionaries-Advanced/P08.SoftuniParty.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P08.SoftuniParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var regularGuest = new HashSet<string>();
+            var vipGuest = new HashSet<string>();
+            string line = Console.ReadLine();
+            while (line != "PARTY")
+            {
+                if (isDigit(line[0]))
+                {
+                    vipGuest.Add(line);
+                }
+                else
+                {
+                    regularGuest.Add(line);
+                }
+                line = Console.ReadLine();
+            }
+
+            line = Console.ReadLine();
+
+            while (line != "END")
+            {
+                if (isDigit(line[0]))
+                {
+                    vipGuest.Remove(line);
+                }
+                else
+                {
+                    regularGuest.Remove(line);
+                }
+                line = Console.ReadLine();
+            }
+
+            int allGuest = vipGuest.Count + regularGuest.Count;
+            Console.WriteLine(allGuest);
+            foreach (var vipPerson in vipGuest)
+            {
+                Console.WriteLine(vipPerson);
+            }
+            foreach (var person in regularGuest)
+            {
+                Console.WriteLine(person);
+            }
+
+        }
+
+        static bool isDigit(char v)
+        {
+            bool isDigit = true;
+            if (v >= 48 && v <= 57)
+            {
+                isDigit = true;
+            }
+            else
+            {
+                isDigit = false;
+            }
+            return isDigit;
+        }
+    }
+}
