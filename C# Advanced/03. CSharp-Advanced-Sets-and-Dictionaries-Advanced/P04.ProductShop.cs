51eda5c0c87a58f36ba9873e0292fdccb08f8f4a
VladimirPelov <vpelov@mail.bg>
2022-05-26 10:23:34 +0300

    Add files via upload

---
+++ C# Advanced/03. CSharp-Advanced-Sets-and-Dictionaries-Advanced/P04.ProductShop.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04.ProductShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var shopDict = new Dictionary<string, Dictionary<string, double>>();
+            string line = Console.ReadLine();
+            while (line != "Revision")
+            {
+                string[] command = line.Split(", ").ToArray();
+                string shop = command[0];
+                string product = command[1];
+                double price = double.Parse(command[2]);
+
+                if (!shopDict.ContainsKey(shop))
+                {
+                    Dictionary<string, double> productDict = new Dictionary<string, double>();
+                    productDict.Add(product, price);
+                    shopDict.Add(shop, productDict);
+                }
+                else
+                {
+                    if (!shopDict[shop].ContainsKey(product))
+                    {
+                        shopDict[shop].Add(product, price);
+                    }
+                    else
+                    {
+                        shopDict[shop][product] = price;
+                    }
+                }
+
+                line = Console.ReadLine();
+            }
+            var printDict = shopDict.OrderBy(x => x.Key);
+
+            foreach (var shop in printDict)
+            {
+                Console.WriteLine($"{shop.Key}-> ");
+                foreach (var item in shop.Value)
+                {
+                    Console.WriteLine($"Product: {item.Key}, Price: {item.Value}");
+                }
+            }
+
+        //Product: apple, Price: 1.2
+        }
+    }
+}
