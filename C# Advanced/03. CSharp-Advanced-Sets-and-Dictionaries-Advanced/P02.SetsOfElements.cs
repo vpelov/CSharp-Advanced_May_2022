7e5ee968a8325dc2a9d7b9d0916ed4241f54576f
VladimirPelov <vpelov@mail.bg>
2022-05-26 15:59:47 +0300

    Add files via upload

---
+++ C# Advanced/03. CSharp-Advanced-Sets-and-Dictionaries-Advanced/P02.SetsOfElements.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02.SetsOfElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int n = numbers[0];
+            int m = numbers[1];
+
+            var nSet = new HashSet<int>();
+            var mSet = new HashSet<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                nSet.Add(number);
+            }
+            for (int j = 0; j < m; j++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                mSet.Add(number);
+            }
+
+            foreach (int number in nSet)
+            {
+                foreach (int num in mSet)
+                {
+                    if (number == num)
+                    {
+                        Console.Write($"{num} ");
+                    }
+                }
+            }
+        }
+    }
+}
