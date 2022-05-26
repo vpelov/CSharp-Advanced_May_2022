5455f8c4d89dd97ee743fd4c0d326a86f1c5d542
VladimirPelov <vpelov@mail.bg>
2022-05-26 09:54:25 +0300

    Add files via upload

---
+++ C# Advanced/03. CSharp-Advanced-Sets-and-Dictionaries-Advanced/P03.Largest3Numbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace P03.Largest3Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArray = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int[] printArray = numbersArray.OrderByDescending(x => x).Take(3).ToArray();
+
+            for (int i = 0; i < printArray.Length; i++)
+            {
+                Console.Write($"{printArray[i]} ");
+            }
+        }
+    }
+}
