5483650a38e3f87938740aab83a02751de7c36b7
VladimirPelov <vpelov@mail.bg>
2022-05-26 15:04:09 +0300

    Add files via upload

---
+++ C# Advanced/03. CSharp-Advanced-Sets-and-Dictionaries-Advanced/P07.ParkingLot.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P07.ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> parkingCar = new HashSet<string>();
+            string line = Console.ReadLine();
+            while (line != "END")
+            {
+                string[] cmd = line.Split(", ");
+                string action = cmd[0];
+                string carNumber = cmd[1];
+
+                if (action == "IN")
+                {
+                    parkingCar.Add(carNumber);
+                }
+                else if(action == "OUT")
+                {
+                    parkingCar.Remove(carNumber);
+                }
+                line = Console.ReadLine();
+            }
+
+            if (parkingCar.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var item in parkingCar)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+        }
+    }
+}
