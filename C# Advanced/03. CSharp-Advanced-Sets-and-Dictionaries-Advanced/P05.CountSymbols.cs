fa3b5ebfc6dbe412170bf3c19464cffe2bb55fbc
VladimirPelov <vpelov@mail.bg>
2022-05-27 08:18:44 +0300

    Add files via upload

---
+++ C# Advanced/03. CSharp-Advanced-Sets-and-Dictionaries-Advanced/P05.CountSymbols.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05.CountSymbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            var countDict = new Dictionary<char, int>();
+            char[] charArray = text.ToCharArray();
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                char currentChar = charArray[i];
+                if (!countDict.ContainsKey(currentChar))
+                {
+                    countDict.Add(currentChar, 1);
+                }
+                else
+                {
+                    countDict[currentChar]++;
+                }
+            }
+
+            var printDict = countDict.OrderBy(x => x.Key);
+            foreach (var chars in printDict)
+            {
+                Console.WriteLine($"{chars.Key}: {chars.Value} time/s");
+            }
+        }
+    }
+}
+//S: 1 time / s
