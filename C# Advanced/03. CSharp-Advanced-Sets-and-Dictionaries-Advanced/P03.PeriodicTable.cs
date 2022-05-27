c8d50da5463311d7839b5cd597c78b8fba6a10b1
VladimirPelov <vpelov@mail.bg>
2022-05-27 08:17:22 +0300

    Add files via upload

---
+++ C# Advanced/03. CSharp-Advanced-Sets-and-Dictionaries-Advanced/P03.PeriodicTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03.PeriodicTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            var chemicalCollection = new HashSet<string>();
+            for (int i = 0; i < number; i++)
+            {
+                string[] currentString = Console.ReadLine()
+                    .Split(' ')
+                    .ToArray();
+                for (int j = 0; j < currentString.Length; j++)
+                {
+                    string current = currentString[j];
+                    chemicalCollection.Add(current);
+                }
+            }
+
+            var printCollection = chemicalCollection.OrderBy(x => x);
+
+            foreach (string elements in printCollection)
+            {
+                Console.Write($"{elements} ");
+            }
+        }
+    }
+}
