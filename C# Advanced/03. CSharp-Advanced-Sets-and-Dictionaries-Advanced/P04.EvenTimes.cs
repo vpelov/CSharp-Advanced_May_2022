9ed2a0416208951b3d8b5759f58c9beccb0537fe
VladimirPelov <vpelov@mail.bg>
2022-05-27 08:18:03 +0300

    Add files via upload

---
+++ C# Advanced/03. CSharp-Advanced-Sets-and-Dictionaries-Advanced/P04.EvenTimes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P04.EvenTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var countDict = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (!countDict.ContainsKey(number))
+                {
+                    countDict.Add(number, 1);
+                }
+                else
+                {
+                    countDict[number]++;
+                }
+            }
+
+            foreach(var evenElement in countDict)
+            {
+                if (evenElement.Value % 2 == 0)
+                {
+                    Console.WriteLine(evenElement.Key);
+                }
+            }
+        }
+    }
+}
