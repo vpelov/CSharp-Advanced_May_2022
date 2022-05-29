60410e18f5f986892e3f50976f26c94eb4c27d8b
VladimirPelov <vpelov@mail.bg>
2022-05-29 21:08:14 +0300

    Add files via upload

---
+++ C# Advanced/04. CSharp-Advanced-Streams-Files-and-Directories/OddLines.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\input.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    int count = 0;
+                    List<string> textList = new List<string>();
+                    string line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        textList.Add(line);
+
+                        line = reader.ReadLine();
+                    }
+                    foreach (var item in textList)
+                    {
+                        if (count % 2 != 0)
+                        {
+                            writer.WriteLine(item);
+                        }
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+}
+
