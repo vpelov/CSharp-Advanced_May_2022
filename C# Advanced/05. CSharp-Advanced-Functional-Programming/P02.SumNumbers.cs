734b22b74a41f0ca6817b628131a534b31eda332
VladimirPelov <vpelov@mail.bg>
2022-05-31 09:28:00 +0300

    Add files via upload

---
+++ C# Advanced/05. CSharp-Advanced-Functional-Programming/P02.SumNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace P02.SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(inputNumbers.Length);
+            Console.WriteLine(inputNumbers.Sum());
+
+
+
+        }
+    }
+}
