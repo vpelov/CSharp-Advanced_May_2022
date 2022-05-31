860448382378bcfec80f87db1af62ed7bae84301
VladimirPelov <vpelov@mail.bg>
2022-05-31 10:02:07 +0300

    Add files via upload

---
+++ C# Advanced/05. CSharp-Advanced-Functional-Programming/P03.CountUppercaseWords2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace P03.CountUppercaseWords
+{
+    internal class Program
+    {
+        static Func<string, bool> isUpperCase = (x) => char.IsUpper(x[0]);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join("\n", Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => isUpperCase(x))));
+        }
+
+    }
+}
