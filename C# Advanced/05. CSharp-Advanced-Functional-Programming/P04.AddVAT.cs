fe596e7f266acbfc5a5b75eafbfe28d409ea065a
VladimirPelov <vpelov@mail.bg>
2022-05-31 10:25:06 +0300

    Add files via upload

---
+++ C# Advanced/05. CSharp-Advanced-Functional-Programming/P04.AddVAT.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace P04.AddVAT
+{
+    internal class Program
+    {
+        static Func<double, double> costWithVAT = x => x * 1.20;
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(string.Join("\n", Console.ReadLine()
+            //    .Split(", ")
+            //    .Select(double.Parse)
+            //    .Select(x => costWithVAT(x))
+            //    .ToArray()));
+
+            double[] inputData = Console.ReadLine()
+                .Split(", ")
+                .Select(double.Parse)
+                .Select(x => costWithVAT(x))
+                .ToArray();
+
+            foreach (var item in inputData)
+            {
+                Console.WriteLine($"{item:f2}");
+            }
+
+        }
+    }
+}
