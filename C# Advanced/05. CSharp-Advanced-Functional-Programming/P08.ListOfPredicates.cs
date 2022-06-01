abe003ed64d48ad5b86bca370a89e68c2d85cd3a
VladimirPelov <vpelov@mail.bg>
2022-06-01 17:21:43 +0300

    Add files via upload

---
+++ C# Advanced/05. CSharp-Advanced-Functional-Programming/P08.ListOfPredicates.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P08.ListOfPredicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = int.Parse(Console.ReadLine());
+            List<int> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Distinct()
+                .ToList();
+            
+            for (int i = 1; i <= maxNumber; i++)
+            {
+                bool isPrint = true;
+                foreach (var item in numbers)
+                {
+                    if (!func(i, item))
+                    {
+                        isPrint = false;
+                        break;
+                    }
+                }
+
+                if (isPrint)
+                {
+                    Console.Write(i + " ");
+                }
+            }       
+        }
+        static Func<int, int, bool> func = (x, y) => x % y == 0;        
+    }
+}
