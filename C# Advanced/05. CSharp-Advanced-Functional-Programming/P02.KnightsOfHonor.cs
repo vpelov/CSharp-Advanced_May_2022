49dbdc7e9508743b15110091ba283275e76c0a00
VladimirPelov <vpelov@mail.bg>
2022-06-01 08:36:55 +0300

    Add files via upload

---
+++ C# Advanced/05. CSharp-Advanced-Functional-Programming/P02.KnightsOfHonor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02.KnightsOfHonor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> inputData = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            Action<string> addAndPrint = str => Console.WriteLine("Sir " + str);
+
+            inputData.ForEach(addAndPrint);           
+        }
+    }
+}
