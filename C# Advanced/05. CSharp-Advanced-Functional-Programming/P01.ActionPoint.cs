e3fe7c23dc9ae26396139b79f2766bb966f9a608
VladimirPelov <vpelov@mail.bg>
2022-06-01 08:18:30 +0300

    Add files via upload

---
+++ C# Advanced/05. CSharp-Advanced-Functional-Programming/P01.ActionPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.ActionPoint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputData = Console.ReadLine()
+                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                 .ToList();
+
+            Action<string> print = str => Console.WriteLine(str);
+
+            inputData.ForEach(print);
+        }
+    }
+}
