fe2f03a915eba9fff5abda76ac216eff191277e4
VladimirPelov <vpelov@mail.bg>
2022-06-01 08:37:29 +0300

    Add files via upload

---
+++ C# Advanced/05. CSharp-Advanced-Functional-Programming/P03.CustomMinFunction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03.CustomMinFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Func<int[], int> getMinNum = num => num.Min();
+
+            Console.WriteLine(getMinNum(numbers));
+        }
+    }
+}
