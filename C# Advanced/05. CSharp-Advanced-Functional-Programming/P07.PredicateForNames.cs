d9a08ee9d6249e88aab1417335843dbe1c464349
VladimirPelov <vpelov@mail.bg>
2022-06-01 14:01:35 +0300

    Add files via upload

---
+++ C# Advanced/05. CSharp-Advanced-Functional-Programming/P07.PredicateForNames.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace P07.PredicateForNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int, bool> func = (x, y) => x.Length <= y;
+            int number = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            foreach (var item in names)
+            {
+                if (func(item, number))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
