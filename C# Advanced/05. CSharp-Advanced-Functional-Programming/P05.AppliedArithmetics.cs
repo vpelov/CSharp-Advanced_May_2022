5347a1d045a8799f5e837848bbc8bb1b72e16d67
VladimirPelov <vpelov@mail.bg>
2022-06-01 12:01:28 +0300

    Add files via upload

---
+++ C# Advanced/05. CSharp-Advanced-Functional-Programming/P05.AppliedArithmetics.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05.AppliedArithmetics
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            Func<int, int> add = x => x + 1;
+            Func<int, int> multiply = x => x * 2;
+            Func<int, int> substract = x => x - 1;
+            Action<int> printElement = x => Console.Write(x + " ");
+
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                if (command == "add")
+                {
+                    numbers = numbers.Select(add).ToList();
+                }
+                else if (command == "multiply")
+                {
+                    numbers = numbers.Select(multiply).ToList();
+                }
+                else if (command == "subtract")
+                {
+                    numbers = numbers.Select(substract).ToList();
+                }
+                else if (command == "print")
+                {
+                    numbers.ForEach(printElement);
+                    Console.WriteLine();
+                }
+
+                command = Console.ReadLine();
+            }           
+        }        
+    }
+}
