3c3a06db7649689af37f4369f968a6dfaf3ad03e
VladimirPelov <vpelov@mail.bg>
2022-06-15 13:48:52 +0300

    Add files via upload

---
+++ C# Advanced/09. CSharp-Advanced-Iterators-and-Comparators/P07.Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace P07.CustomComarator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int, int, int> comparator = (first, second) =>
+            {
+                if (first % 2 == 0 && second % 2 != 0)
+                {
+                    return -1;
+                }
+                else if (first % 2 != 0 && second % 2 == 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return first.CompareTo(second);
+                }
+            };
+
+            Array.Sort(numbers, new Comparison<int>(comparator));
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
