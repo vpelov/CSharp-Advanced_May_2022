4137166bfdb1581bd56d969937e1a9ed66b24997
VladimirPelov <vpelov@mail.bg>
2022-07-03 20:17:17 +0300

    Add files via upload

---
+++ C# OOP/01. CSharp-OOP-Inheritance-Exercise/P05.Restaurant/Dessert.cs
@@ -0,0 +1,14 @@
+﻿namespace Restaurant
+{
+    public class Dessert : Food
+    {
+        public Dessert(string name, decimal price, double grams, double Calories)
+            : base(name, price, grams)
+        {
+            this.Calories = Calories;
+        }
+
+        public double Calories { get; set; }
+
+    }
+}
