4e354aa560b0e311014ba4a9c2152601d1dc86e0
VladimirPelov <vpelov@mail.bg>
2022-07-06 19:28:24 +0300

    Add files via upload

---
+++ C# OOP/02. CSharp-OOP-Encapsulation-Lab/P04.Team/Team.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    class Team
+    {
+
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.Name = name;
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public IReadOnlyCollection<Person> FirstTeam
+        {
+            get { return this.firstTeam.AsReadOnly(); }
+        }
+
+        public IReadOnlyCollection<Person> ReserveTeam
+        { 
+            get { return this.reserveTeam.AsReadOnly(); }
+        }
+
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+
+    }
+}
