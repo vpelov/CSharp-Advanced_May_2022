78d2d7452d0e555465b19f8a0afaf774dcd83b2d
VladimirPelov <vpelov@mail.bg>
2022-07-08 16:54:04 +0300

    Add files via upload

---
+++ C# OOP/02. CSharp-OOP-Encapsulation-Exercise/P03.ShoppingSpree/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03.ShoppingSpree
+{
+    class Person
+    {
+        private string name;
+        private decimal money;
+        public List<Product> bag;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.bag = new List<Product>();
+        }
+
+        public string Name 
+        {
+            get { return this.name; }
+            set 
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                else
+                {
+                    this.name = value;
+                }
+            }
+        }
+
+        public decimal Money
+        {
+            get { return this.money; } 
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                else
+                {
+                    this.money = value;
+                }
+            }
+        }
+
+
+
+
+        public void AddProduct(Product product)
+        {
+            this.bag.Add(product);
+        }
+    }
+}
---
+++ C# OOP/02. CSharp-OOP-Encapsulation-Exercise/P03.ShoppingSpree/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03.ShoppingSpree
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Person> persons = new List<Person>();
+            List<Product> products = new List<Product>();
+
+            try
+            {
+                string[] inDataPerson = Console.ReadLine()
+                .Split(';', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+                for (int i = 0; i < inDataPerson.Length; i++)
+                {
+                    string[] currPeople = inDataPerson[i]
+                        .Split('=', StringSplitOptions.RemoveEmptyEntries)
+                        .ToArray();
+                    string name = currPeople[0];
+                    decimal money = decimal.Parse(currPeople[1]);
+                    Person person = new Person(name, money);
+                    persons.Add(person);
+                }
+
+                string[] inDataProducts = Console.ReadLine()
+                    .Split(';', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                for (int i = 0; i < inDataProducts.Length; i++)
+                {
+                    string[] currProduct = inDataProducts[i]
+                        .Split('=', StringSplitOptions.RemoveEmptyEntries)
+                        .ToArray();
+                    string productName = currProduct[0];
+                    decimal cost = decimal.Parse(currProduct[1]);
+                    Product product = new Product(productName, cost);
+                    products.Add(product);
+                }
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+                if (ae.Message == "Money cannot be negative")
+                {
+                    return;
+                }
+            }
+
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+                string[] currentCommand = command
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string name = currentCommand[0];
+                string product = currentCommand[1];
+                Person currPerson = persons.First(p => p.Name == name);
+                Product currProduct = products.First(p => p.Name == product);
+                if (currPerson.Money >= currProduct.Cost)
+                {
+                    currPerson.AddProduct(currProduct);
+                    currPerson.Money -= currProduct.Cost;
+                    Console.WriteLine($"{currPerson.Name} bought {currProduct.Name}");
+                }
+                else
+                {
+                    Console.WriteLine($"{currPerson.Name} can't afford {currProduct.Name}");
+                }
+                command = Console.ReadLine();
+            }
+
+            foreach (Person person in persons)
+            {
+                if (person.bag.Count == 0 )
+                {
+                    Console.WriteLine($"{person.Name} - Nothing bought");
+                }
+                else
+                {
+                    Console.WriteLine($"{person.Name} - {string.Join(", ", person.bag)}");
+                }
+            }
+
+        }
+    }
+}
