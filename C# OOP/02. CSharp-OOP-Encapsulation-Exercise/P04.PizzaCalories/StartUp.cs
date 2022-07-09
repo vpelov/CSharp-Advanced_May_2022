86535f35ef4423ba6c321a9ed9df5d84df943cd7
VladimirPelov <vpelov@mail.bg>
2022-07-09 17:26:58 +0300

    Add files via upload

---
+++ C# OOP/02. CSharp-OOP-Encapsulation-Exercise/P04.PizzaCalories/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace P04.PizzaCalories
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                string[] pizzaCmd = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string[] doughCmd = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string pizzaName = pizzaCmd[1];
+
+                string flourType = doughCmd[1];
+                string flourTehnik = doughCmd[2];
+                int grams = int.Parse(doughCmd[3]);
+                Dough dough = new Dough(flourType, flourTehnik, grams);
+
+                Pizza pizza = new Pizza(pizzaName, dough);
+
+                string command = Console.ReadLine();
+
+                while (command != "END")
+                {
+                    string[] input = command.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                    string toppingType = input[1];
+                    int toppingGrams = int.Parse(input[2]);
+                    Topping topping = new Topping(toppingType, toppingGrams);
+                    pizza.AddTopping(topping);
+
+                    command = Console.ReadLine();
+                }
+
+                Console.WriteLine($"{pizzaName} - {pizza.TotalCalories:f2} Calories.");
+            }
+            catch (ArgumentException ae)
+            {
+
+                Console.WriteLine(ae.Message);
+            }            
+        }
+    }
+}
---
+++ C# OOP/02. CSharp-OOP-Encapsulation-Exercise/P04.PizzaCalories/Topping.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P04.PizzaCalories
+{
+    class Topping
+    {
+        private readonly Dictionary<string, double> toppingType = new Dictionary<string, double>()
+        {
+            { "meat", 1.2},
+            { "veggies", 0.8},
+            { "cheese", 1.1},
+            { "sauce", 0.9}
+        };
+
+        private const double caloriesPerGram = 2;
+
+        private string type;
+        private double grams;
+
+        public Topping(string type, double grams)
+        {
+            this.Type = type;
+            this.Grams = grams;
+        }
+
+        public string Type 
+        { 
+            get => type;
+            set
+            {
+                if (toppingType.ContainsKey(value.ToLower()))
+                {
+                    this.type = value;
+                }
+                else
+                {
+                    throw new ArgumentException($"Cannot place {value} on top of your pizza.");
+                }
+            }
+        }
+
+        public double Grams
+        { 
+            get => grams;
+            set
+            {
+                if (value >= 1 && value <= 50)
+                {
+                    this.grams = value;
+                }
+                else
+                {
+                    throw new ArgumentException($"{this.type} weight should be in the range [1..50].");
+                }
+            }
+        }
+
+
+        public double CaloriesTopping()
+        {
+            return caloriesPerGram * toppingType[this.type.ToLower()] * this.grams;
+        }
+    }
+}
