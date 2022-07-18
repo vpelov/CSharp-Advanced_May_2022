1b017d7689d35bbe26d40cfbd3a66366796fe9f5
VladimirPelov <vpelov@mail.bg>
2022-07-18 07:39:22 +0300

    Add files via upload

---
+++ C# OOP/05. CSharp-OOP-Exceptions-and-Error-Handling/P01.SquareRoot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace P01.SquareRoot
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            int num = int.Parse(Console.ReadLine());
+
+            try
+            {
+                double sqr = TakeSquare(num);
+                Console.WriteLine(sqr);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+
+        }
+
+        public static double TakeSquare(int num)
+        {
+            if (num < 0)
+            {
+                throw new ArgumentException("Invalid number.");
+            }
+
+            double result = Math.Sqrt(num);
+            return result;
+
+        }
+    }
+}
